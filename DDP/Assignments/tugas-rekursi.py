062dd35ca033f385961e00421648c46e8ae2cbec
Baihaki Al Biruni <baihakitanjung12@gmail.com>
2021-12-18 08:57:53 +0700

    feat: pw7, ddp assignment

---
+++ DDP/Assignments/tugas-rekursi.py
@@ -0,0 +1,18 @@
+# Tugas 1
+# faktorial
+n = int(input('Masukkan nilai n: '))
+
+def hitung_faktorial (n):
+  if n > 2:
+    return n * hitung_faktorial(n - 1)
+
+  return 2
+
+faktorial = hitung_faktorial(n)
+print(f'{n}! = {faktorial}')
+
+
+
+
+
+
--- DDP/Assignments/tugas-searching.py
+++ DDP/Assignments/tugas-searching.py
@@ -1,20 +1,41 @@
 # tugas 1
-hasil_akhir = [
-    {'nama': 'Reza', 'nilai': 70},
-    {'nama': 'Ciut', 'nilai': 63},
-    {'nama': 'Dian', 'nilai': 80},
-    {'nama': 'Badu', 'nilai': 40}
-]
+# hasil_akhir = [
+#     {'nama': 'Reza', 'nilai': 70},
+#     {'nama': 'Ciut', 'nilai': 63},
+#     {'nama': 'Dian', 'nilai': 80},
+#     {'nama': 'Badu', 'nilai': 40}
+# ]
 
 
-def find_by_name(nama, hasil_akhir):
-    for i in hasil_akhir:
-        if i['nama'] == nama:
-            return i
-    return None
+# def find_by_name(nama, hasil_akhir):
+#     for i in hasil_akhir:
+#         if i['nama'] == nama:
+#             return i
+#     return None
 
 
-print(find_by_name('Dian', hasil_akhir))
+# print(find_by_name('Dian', hasil_akhir))
 
 
 # tugas 2
+
+def binary_search_recursive(list, target, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+    if target == list[mid]:
+        return mid
+
+    if target < list[mid]:
+        return binary_search_recursive(list, target, start, mid-1)
+    else:
+        return binary_search_recursive(list, target, mid+1, end)
+
+
+target = 18
+list = [1, 2, 5, 7, 13, 15, 16, 18, 24, 28, 29]
+
+print("Searching for {}".format(target))
+print("Index of {} : {}".format(
+    target, binary_search_recursive(list, target, 0, len(list))))
