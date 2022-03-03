29550a76e8a9a8a8cb1c6fff9f804d72e5c53599
Baihaki Al Biruni <baihakitanjung12@gmail.com>
2022-03-03 22:34:28 +0700

    feat: praktikum semester 2 and fixing folder

--- DDP/Assignments/api/api.py
+++ DDP/Assignments/api/api.py
@@ -1,41 +1,56 @@
-import requests
-from tabulate import tabulate
-from os import system
+# import requests
+# from tabulate import tabulate
+# from os import system
 
 
-class Pokemon:
+# class Pokemon:
 
-    offset = 0
-    limit = 100
-    total_data = 10
-    url = "https://pokeapi.co/api/v2/ability/?offset=" + \
-        str(offset) + "&limit=" + str(limit)
-    data = []
+#     offset = 0
+#     limit = 100
+#     total_data = 10
+#     url = "https://pokeapi.co/api/v2/ability/?offset=" + \
+#         str(offset) + "&limit=" + str(limit)
+#     data = []
 
-    def getPokemonData(self, order="ASC", page=1):
-        header = ['No', 'Nama Pokemon', 'URL']
-        response = requests.get(self.url).json()['results']
-        self.data = []
-        for i in range(len(response)):
-            self.data.append([i+1, response[i]['name'], response[i]['url']])
-        start = self.total_data * (page - 1)
-        self.data = self.data[start:start+self.total_data]
-        self.data = sorted(
-            self.data, reverse=True if order == "DESC" else False)
-        print(tabulate(self.data, header, tablefmt="github"))
+#     def getPokemonData(self, order="ASC", page=1):
+#         header = ['No', 'Nama Pokemon', 'URL']
+#         response = requests.get(self.url).json()['results']
+#         self.data = []
+#         for i in range(len(response)):
+#             self.data.append([i+1, response[i]['name'], response[i]['url']])
+#         start = self.total_data * (page - 1)
+#         self.data = self.data[start:start+self.total_data]
+#         self.data = sorted(
+#             self.data, reverse=True if order == "DESC" else False)
+#         print(tabulate(self.data, header, tablefmt="github"))
 
-    def preview(self):
-        system('cls')
-        print("="*16)
-        print("DATA POKEMON")
-        print("="*16)
-        print()
-        order = input("Urutan Data ASC/DESC : ").upper()
-        page = int(input("Halaman : "))
-        self.getPokemonData(order, page)
-        again = input("Lihat Data Lainnya (Y/N) : ").upper()
-        if again == "Y":
-            self.preview()
+#     def preview(self):
+#         system('cls')
+#         print("="*16)
+#         print("DATA POKEMON")
+#         print("="*16)
+#         print()
+#         order = input("Urutan Data ASC/DESC : ").upper()
+#         page = int(input("Halaman : "))
+#         self.getPokemonData(order, page)
+#         again = input("Lihat Data Lainnya (Y/N) : ").upper()
+#         if again == "Y":
+#             self.preview()
 
 
-Pokemon().preview()
+# Pokemon().preview()
+
+
+kolum = int(12)
+baris = int(7)
+x = (kolum // 2) - 1
+y = 2
+print("." * kolum, end="\n")
+while x != 0:
+    while y <= (baris + 3):
+        print("." * x, end="")
+        print("*" * y, end="")
+        print("." * x, end="\n")
+        x -= 1
+        y += 2
+print("*" * kolum)
---
+++ Pemogramman Web/Semester 2/baihakialbiruni_praktikum01/array_siswa.php
@@ -0,0 +1,55 @@
+ <?php
+    $ns1 = ['id' => 1, 'nim' => '01101', 'uts' => 80, 'uas' => 84, 'tugas' => 78];
+    $ns2 = ['id' => 2, 'nim' => '01121', 'uts' => 70, 'uas' => 50, 'tugas' => 68];
+    $ns3 = ['id' => 3, 'nim' => '01130', 'uts' => 60, 'uas' => 86, 'tugas' => 70];
+    $ns4 = ['id' => 4, 'nim' => '01134', 'uts' => 90, 'uas' => 91, 'tugas' => 82];
+
+    $ar_nilai = [$ns1, $ns2, $ns3, $ns4];
+
+    ?>
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ </head>
+
+ <body>
+     Web Programming 02| STT-NF 9
+     Praktikum 01: PHP Dasar
+     <h3>Daftar Nilai Siswa</h3>
+     <table class="table table-dark" width="100%">
+         <thead>
+             <tr>
+                 <th>No</th>
+                 <th>NIM</th>
+                 <th>UTS</th>
+                 <th>UAS</th>
+                 <th>Tugas</th>
+                 <th>Nilai Akhir</th>
+             </tr>
+         </thead>
+         <tbody>
+             <?php
+                $nomor = 1;
+                foreach ($ar_nilai as $ns) {
+                    echo '<tr><td>' . $nomor . '</td>';
+                    echo '<td>' . $ns['nim'] . '</td>';
+                    echo '<td>' . $ns['uts'] . '</td>';
+                    echo '<td>' . $ns['uas'] . '</td>';
+                    echo '<td>' . $ns['tugas'] . '</td>';
+                    $nilai_akhir = ($ns['uts'] + $ns['uas'] + $ns['tugas']) / 3;
+                    echo '<td>' . number_format($nilai_akhir, 2, ',', '.') . '</td>';
+                    echo '<tr/>';
+                    $nomor++;
+                }
+                ?>
+         </tbody>
+     </table>
+ </body>
+
+ </html>
